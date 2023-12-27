868bd1489fdeb4598f19f64366a764c2e6e5937e
Michael Weiß <weiss_michael@aon.at>
2023-12-27 14:18:31 +0100

    bill capturing works

--- billApplication/view/billFrame.py
+++ billApplication/view/billFrame.py
@@ -85,7 +85,7 @@ class BillFrame(tk.Frame):
 
         while True:
             ret, frame = cap.read()
-            frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+            #frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
             cv2.imshow('Input', frame)
 
             c = cv2.waitKey(1)

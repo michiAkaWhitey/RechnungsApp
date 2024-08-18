341c1797cb077ef6e4d5b0ea8b93b57c1985c4e8
Michael Weiß <michael.weiss@jku.at>
2024-08-18 15:29:59 +0200

    http authentification added

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {"admin": generate_password_hash("cr7_V+8")}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+@auth.login_required
+def index():
+    return render_template("index.html") 
 
-# main driver function
 if __name__ == '__main__':
     app.run()

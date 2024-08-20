aaa5ff8fc8921010ac1206e96bb44072dd79011b
Michael Weiß <michael.weiss@jku.at>
2024-08-20 20:43:40 +0200

    bug fix

--- app.py
+++ app.py
@@ -9,15 +9,13 @@ auth = HTTPBasicAuth()
 users = {"admin": generate_password_hash("admin")}
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
+    if username in users and check_password_hash(users.get(username), password):
         return username
 
 @app.route('/')
 @auth.login_required
 def index():
-    companies = db.companyNames()
-    return render_template("index.html", companies=companies) 
+    return render_template("index.html") 
 
 if __name__ == '__main__':
     app.run(debug=True)
